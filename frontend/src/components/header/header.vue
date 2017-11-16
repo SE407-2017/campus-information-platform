65ecc6f7987edcbbde1f2746b59e8577de1f31a3
shinytang6 <1074461480@qq.com>
2017-11-16 14:37:30 +0800

    update header style

--- frontend/src/components/header/header.vue
+++ frontend/src/components/header/header.vue
@@ -1,10 +1,17 @@
 <template>
 <div class="header">
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-      <el-menu-item index="1" class="name" disabled>校园信息平台</el-menu-item>
+      <div class="title-wrapper">
+        <el-menu-item index="1" @click.prevent.self class="title">校园信息平台</el-menu-item>
+      </div>
       <!-- <el-menu-item index="2"></el-menu-item> -->
-      <el-menu-item index="2">活动</el-menu-item>
-      <div class="state">
+      <div class="menu-wrapper">
+        <el-menu-item index="2">活动</el-menu-item>
+      </div>
+      <div class="search-wrapper">
+          <el-input v-model="input" placeholder="请输入内容" class="input"></el-input>
+      </div>
+      <div class="state-wrapper">
           <el-menu-item index="3">登录</el-menu-item>
           <el-menu-item index="4">注册</el-menu-item>
           <el-menu-item index="5">注销</el-menu-item>
@@ -34,8 +41,19 @@ export default {
 @import url("//unpkg.com/element-ui@2.0.4/lib/theme-chalk/index.css");
 .header
     margin-top: -60px
-    .name
-        font-size: 20px    
-    .state
+    .title-wrapper
+        display: inline-block
+        float: left
+        .title
+            font-size: 20px    
+    .menu-wrapper
+        display: inline-block
+        float: left
+    .search-wrapper
+        display: inline-block
+        margin-top: 10px
+
+    .state-wrapper
+       display: inline-block
        float: right 
 </style>
