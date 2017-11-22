a3257ae57cb87fc30a1341820a24049a29375d95
Llmoment <weiyuh@outlook.com>
2017-11-22 11:42:37 +0000

    add Admin

---
+++ backend/database/migrations/2017_11_22_113820_create_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string("adminname")->unique();
+          $table->string("email")->nullable()->unique();
+          $table->string("phone")->unique()->nullable();
+          $table->string("password");
+          $table->string("avatar_url")->nullable();
+          $table->text("intro")->nullable();
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('admins');
+    }
+}
