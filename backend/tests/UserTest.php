68deb7a2295cd0ff8f82dfdd3413a172582d6bda
nashiluoyuji <31882978+nashiluoyuji@users.noreply.github.com>
2017-11-28 22:23:28 +0800

    add files

--- backend/tests/UserTest.php
+++ backend/tests/UserTest.php
@@ -13,7 +13,30 @@ class UserTest extends TestCase
      */
     public function testExample()
     {
-        $response = $this->call('GET', 'localhost:8086/api/activity/read');
+            $response = $this->call('GET', 'localhost:8086/api/admin/login');
     $this->assertEquals(200, $response->status());
-    }
+    $response = $this->call('GET', '/api/admin/logout');
+    $this->assertEquals(200, $response->status());
+    $response = $this->call('GET', '/api/admin/check');
+    $this->assertEquals(200, $response->status());
+     
+
+    $response = $this->call('GET', '/api/activity/add');
+    $this->assertEquals(200, $response->status());
+    $response = $this->call('GET', '/api/activity/update');
+    $this->assertEquals(200, $response->status());
+
+    $response = $this->call('GET', '/api/activity/read');
+    $this->assertEquals(200, $response->status());
+    $response = $this->call('GET', '/api/activity/remove');
+    $this->assertEquals(200, $response->status());
+
+
+
+    $response = $this->call('GET', '/api/comment/add');
+    $this->assertEquals(200, $response->status());
+    $response = $this->call('GET', '/api/comment/read');
+    $this->assertEquals(200, $response->status());
+ 
+     }
 }
